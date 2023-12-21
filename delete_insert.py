ee1c9a687a3bda6b49f04b43238c1ea9a75850e5
affiqazrin <53438635+affiqazrin@users.noreply.github.com>
2023-12-21 16:08:42 +0800

    Update delete_insert.py

--- delete_insert.py
+++ delete_insert.py
@@ -1,16 +1,27 @@
-#delete
-try:
-    DELETE_RECORD=pd.read_sql_query("DELETE FROM "+params['to_temp_table']+" WHERE Month="+f"'"{params['Month']}', engine)
-except:
-    print('Records: '+f'{params['Month']}'+' deleted successfully')
-    #df=pd.read_sql("SELECT * FROM "+params[params_list[1]]+" WHERE Month=%(month)s LIMIT 10", engine)
-#df
+import pandas as pd
+from sqlalchemy import create_engine
 
-#insert
-try:
-    d['Sheet1'][list(rename_cols.values())].to_sql(con=engine, name=params['to_temp_table'], if_exists='replace', index=False)
-    print(d['Sheet1'][list(rename_cols.values())].columns)
-    print(d['Sheet1'][list(rename_cols.values())].shape)
-except:
-    print("OE: ", e)
-    print("Error Code: ", e.orig.args[0]) 
+def delete_and_append_records(df, target_month, table_name, engine):
+    try:
+        # Delete records for the specified month
+        delete_query = f"DELETE FROM {table_name} WHERE Month = '{target_month}'"
+        pd.read_sql_query(delete_query, engine)
+        print(f"Records for Month: {target_month} deleted successfully")
+    except Exception as delete_error:
+        print("Error deleting records:", delete_error)
+        return
+
+    try:
+        # Append new records to the table
+        df.to_sql(con=engine, name=table_name, if_exists='append', index=False)
+        print("Records appended successfully")
+    except Exception as append_error:
+        print("Error appending records:", append_error)
+
+# Example usage:
+# Replace 'your_database_url' with your actual MySQL database URL
+database_url = 'mysql://username:password@localhost/your_database_name'
+engine = create_engine(database_url)
+
+# Assuming you have a DataFrame df, a target_month, and a table_name
+delete_and_append_records(df, '2023-01', 'your_table_name', engine)
