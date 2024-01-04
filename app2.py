f6004a00793356ed56edeef4ed9df2d57247b1eb
affiqazrin <53438635+affiqazrin@users.noreply.github.com>
2024-01-04 15:27:02 +0800

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,39 @@
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    # Clear cache, memory, and session when re-uploading new files
+    global decrypted_workbook, data, sql_query
+    decrypted_workbook = io.BytesIO()
+    data = {}
+    sql_query = None
+    session.clear()
+
+    df = {}
+    lst_sheet = []
+    worksheet_names = None
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
+
+        file = request.files['file']
+        print(file)
+        if file.filename == '':
+            return "No selected file"
+
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            try:
+                df, lst_sheet, f = processor.read_insx_file(filepath, decrypted_workbook)
+                print(lst_sheet)
+                session['f'] = f
+
+            except Exception as e:
+                flash(f"An error occurred while reading the file: {str(e)}", 'error')
+                return redirect(url_for('index'))
+
+            return render_template('upload_single.html', worksheet_names=lst_sheet, uploaded_file=decrypted_workbook,
+                                   categories=CATEGORIES, months=MONTHS)
+
+    return render_template('upload_single.html', worksheet_names=lst_sheet, uploaded_file=None, categories=CATEGORIES, months=MONTHS)
