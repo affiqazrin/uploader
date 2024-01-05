a4cf15391fc27bf7d125c41051f6b144dcd2a390
affiqazrin <53438635+affiqazrin@users.noreply.github.com>
2024-01-05 10:49:32 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -11,3 +11,23 @@
                     except Exception as e:
                         df_filtered.loc[:, col] = df_filtered.loc[:, col].astype(str)
                         print(f'Error converting column {col} to float: {e}')
+
+
+
+# Convert value column to float data type
+convert_lst = ['RP', 'rp', 'RM', 'rm', 'CC', 'cc', 'tgt', 'ach', 'act', 'rate', 'payout',
+               'ye_rate', 'amt', 'amount', 'deduct', 'rem1', 'rem2', 'rem3', 'rem4', 'total',
+               'rm', 'ret', 'oth_clw', 'cf_clw']
+
+for col in df_filtered.columns:
+    for convert_item in convert_lst:
+        if convert_item in col:
+            try:
+                # Check if the conversion to float is successful
+                converted_values = pd.to_numeric(df_filtered.loc[:, col], errors='coerce')
+                if not converted_values.isna().any():
+                    df_filtered.loc[:, col] = converted_values.astype(float)
+                else:
+                    print(f'Warning: Unable to convert column {col} to float. Retaining original data type.')
+            except Exception as e:
+                print(f'Error converting column {col} to float: {e}. Retaining original data type.')
