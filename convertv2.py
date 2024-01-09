bca701b858565a072b692b81ad9b180cf0766b1d
affiqazrin <53438635+affiqazrin@users.noreply.github.com>
2024-01-09 10:49:23 +0800

    Update convertv2.py

--- convertv2.py
+++ convertv2.py
@@ -1,4 +1,6 @@
-def convert_columns_to_float(self, convert_lst):
+import pandas as pd
+
+def convert_columns_to_float(df, convert_lst):
     """
     Convert specified columns in the DataFrame to float data type.
     If not possible, retain the original data type. Convert None values to 0.
@@ -18,15 +20,15 @@ def convert_columns_to_float(self, convert_lst):
                 try:
                     # Convert None values to 0
                     df.loc[df[col].isna(), col] = 0
-                    
+
                     # Check if the conversion to float is successful
                     converted_values = pd.to_numeric(df.loc[:, col], errors='coerce')
-                    
+
                     if not converted_values.isna().any():
                         df.loc[:, col] = converted_values.astype(float)
                     else:
                         print(f'Warning: Unable to convert column {col} to float. Retaining original data type.')
                 except Exception as e:
                     print(f'Error converting column {col} to float: {e}. Retaining original data type.')
-                    
+
     return df
